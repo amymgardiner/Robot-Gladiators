812a106c397d43cbbfd3be0ec5ac36306309a8be
Amy Gardiner <amymgardiner@gmail.com>
2022-05-23 21:45:42 -0500

    added high score function

--- assets/js/game.js
+++ assets/js/game.js
@@ -161,13 +161,23 @@ endGame();
 var endGame = function() {
   window.alert("The game has now ended. Let's see how you did!");
 
-  // if player is still alive, player wins!
-  if (playerInfo.health > 0) {
-    window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + " . ");
+  // check local storage for high sore, if it's not there, use 0
+  var highScore = localStorage.getItem("highscore");
+
+  if (highScore === null) {
+    highScore = 0;
+  }
+
+  // if player has more money than the high score, player has a new high score!
+  if (playerInfo.money > highScore) {
+    localStorage.setItem("highscore", playerInfo.money);
+    localStorage.setItem("name", playerInfo.name);
+
+    alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
   }
 
   else {
-    window.alert("You've lost your robot in battle.");
+    alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
   }
 
   // ask player if they'd like to play again
@@ -185,8 +195,7 @@ var endGame = function() {
 var shop = function() {
 
 // ask player what they'd like to do
-var shopOptionPrompt = window.prompt(
-  "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE.");
+var shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE.");
 
 shopOptionPrompt = parseInt(shopOptionPrompt);
 // use switch case to carry out action
